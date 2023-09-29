b01eb3edf2d5833212f9ce4aea07be1694d1cbd8
enessancar <enes57751907@gmail.com>
2023-09-29 10:39:53 +0300

    I creared Attribute View

--- FoodApp/Core/Components/Views/AttributeView.swift
+++ FoodApp/Core/Components/Views/AttributeView.swift
@@ -5,4 +5,48 @@
 //  Created by Enes Sancar on 28.09.2023.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class AttributeView: UIView {
+    
+    private var headerLabel: UILabel!
+    private var bodyLabel: UILabel!
+    
+    init(headerText: String, bodyText: String) {
+        super.init(frame: .zero)
+        
+        configureHeaderLabel(headerText: headerText)
+        configureBodyLabel(bodyText: bodyText)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureHeaderLabel(headerText: String) {
+        headerLabel = UILabel(frame: .zero)
+        addSubview(headerLabel)
+        
+        headerLabel.text = headerText
+        headerLabel.font = .systemFont(ofSize: 16)
+        headerLabel.textColor = .secondaryLabel
+        
+        headerLabel.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview()
+        }
+    }
+    
+    private func configureBodyLabel(bodyText: String) {
+        bodyLabel = UILabel(frame: .zero)
+        addSubview(bodyLabel)
+        
+        bodyLabel.text = bodyText
+        bodyLabel.font = .boldSystemFont(ofSize: 20)
+        bodyLabel.textColor = .secondaryLabel
+        
+        bodyLabel.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
