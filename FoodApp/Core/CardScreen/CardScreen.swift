4e1058d3044fb3eb2c4b6760cf23330c75c9acf9
enessancar <enes57751907@gmail.com>
2023-10-01 23:01:41 +0300

    I created card screen and card view model

--- FoodApp/Core/CardScreen/CardScreen.swift
+++ FoodApp/Core/CardScreen/CardScreen.swift
@@ -5,4 +5,113 @@
 //  Created by Enes Sancar on 1.10.2023.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class CardScreen: UIViewController {
+    
+    private var emptyStateView: EmptyStateView!
+    private var tableView = UITableView(frame: .zero)
+    private var cardConfirmView: CardConfirmView!
+    
+    private var viewModel = CardViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Food in Card"
+        navigationItem.backButtonTitle = "Back"
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        configureEmptyStateView()
+        configureCardConfirmView()
+        configureTableView()
+        addBinders()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.getProducts()
+    }
+    
+    private func addBinders() {
+        viewModel.products.bind { [weak self] products in
+            guard let self else { return }
+            if products.isEmpty {
+                self.view.bringSubviewToFront(emptyStateView)
+            } else {
+                self.tableView.reloadData()
+                self.cardConfirmView.setPrice(totalPrice: self.viewModel.totalPrice)
+                
+                view.bringSubviewToFront(tableView)
+                view.bringSubviewToFront(cardConfirmView)
+            }
+        }
+    }
+    
+    private func configureEmptyStateView() {
+        emptyStateView = EmptyStateView(title: "Your card is empty! 🥲", message: "Let's add some delicious dishes!")
+        view.addSubview(emptyStateView)
+        
+        emptyStateView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension CardScreen {
+    private func configureTableView() {
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CardCell.self, forCellReuseIdentifier: CardCell.identifier)
+        
+        tableView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(cardConfirmView.snp.top)
+        }
+    }
+    
+    private func configureCardConfirmView() {
+        cardConfirmView = CardConfirmView(delegate: self)
+        view.addSubview(cardConfirmView)
+        
+        cardConfirmView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-5)
+        }
+    }
+}
+
+extension CardScreen: CardConfirmCiewProtocol {
+    func confirmButtonPressed() {
+        viewModel.products.value.removeAll()
+        ProductStore.removeAll()
+    }
+}
+
+//MARK: - TableView
+extension CardScreen: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.products.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CardCell.identifier, for: indexPath) as? CardCell else {
+            return UITableViewCell()
+        }
+        cell.configure(viewModel.products.value[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = ProductDetailVC(product: viewModel.products.value[indexPath.row])
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
--- FoodApp/Core/CardScreen/CardViewModel.swift
+++ FoodApp/Core/CardScreen/CardViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class CardViewModel {
+    
+    var products: Observable<[ProductData]> = Observable([])
+    
+    var totalPrice: Double {
+        products.value.reduce(0) { partialResult, product in
+            if product.campaignPrice != nil {
+                return partialResult + (product._campaignPrice)
+            }
+            return partialResult + (product._price)
+        }
+    }
+    
+    func getProducts() {
+        ProductStore.retrieveProducts { [weak self] result in
+            guard let self else { return }
+            
+            switch result {
+            case .success(let products):
+                DispatchQueue.main.async {
+                    self.products.value = products
+                }
+            case .failure(let failure):
+                print(failure.localizedDescription)
+            }
+        }
+    }
+}
